7c361c0209600f4a5d5697bb97dcf04444560140
haanirashid <haaniirashid@gmail.com>
2024-09-11 17:05:44 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+console.log("testing");
+
+
+var domName = document.getElementById("naam");
+var domemail = document.getElementById("mail");
+var dompass = document.getElementById("pass");
+var nameArray = [];
+var emailArray = [];
+var passArray = [];
+
+function clickData() {
+    var inputName = document.getElementById("name").value;
+    var inputEmail = document.getElementById("email").value;
+    var inputPassword = document.getElementById("password").value;
+    nameArray.push(inputName);
+    emailArray.push(inputEmail);
+    passArray.push(inputPassword);
+    console.log(nameArray);
+    console.log(emailArray);
+    console.log(passArray);
+    domName.innerHTML = nameArray;
+    domemail.innerHTML = emailArray;
+    dompass.innerHTML = passArray;
+};
+
+function submitData(event) {
+    var inputName = document.getElementById("name").value;
+    var inputEmail = document.getElementById("email").value;
+    var inputPassword = document.getElementById("password").value;
+    nameArray.push(inputName);
+    emailArray.push(inputEmail);
+    passArray.push(inputPassword);
+    domName.innerHTML = nameArray;
+    domemail.innerHTML = emailArray;
+    dompass.innerHTML = passArray;
+    event.preventDefault();
+}
